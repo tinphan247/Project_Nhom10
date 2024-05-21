339d98bdf66affb6ad456ded7530f89ff9f86241
tinphan247 <151385025+tinphan247@users.noreply.github.com>
2024-05-21 08:51:04 +0700

    Create project.h

---
+++ project.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct SinhVien
+{
+	int stt;
+	int mssv;
+	char* ten;
+	char* ho;
+	char* gender;
+	int ngay, thang, nam;
+	long int cccd;
+	SinhVien* next;
+};
+struct Lop
+{
+	SinhVien* danhsachsv;
+	Lop* loptieptheo;
+	char tenlop[20];
+};
+struct hocky
+{
+	int thutu;
+	char ngaybatdau[11];
+	char ngayketthuc[11];
+	char chonhocky[30];
+	hocky* kitieptheo;
+};
+struct namhoc
+{
+	char thoigiannamhoc[30];
+	hocky* Hocky;
+	namhoc* tieptheo;
+};
+struct listnamhoc
+{
+	namhoc* phead;
+};
+bool kiemtrangaysinh(int ngay, int thang, int nam);
+void taosv(int &STT, int &MSSV, char ten[], char ho[], char gender[], int &ngay, int &thang, int &nam,long int &cccd);
+void themsvvaolop(Lop& lop, int& STT, int& MSSV, char ten[], char ho[], char gender[], int& ngay, int& thang, int& nam,long int& cccd);
+void taonamhoc( listnamhoc& L,namhoc*& N);
+void taohocky(hocky*& H, listnamhoc& L);
