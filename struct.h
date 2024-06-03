b19ba2b95321fa443b837b9f772076ddd2e8f3b4
tinphan247 <151385025+tinphan247@users.noreply.github.com>
2024-06-03 13:25:56 +0700

    Create struct.h

---
+++ struct.h
@@ -0,0 +1,92 @@
+struct Date {
+	int year;
+	int month;
+	int day;
+	string wDay;
+};
+struct User {
+	string id;
+	string password;
+	string lastName;
+	string firstName;
+	string className;
+	string gender;
+	int academicYear;
+	bool isStaff;
+	Date dateOfBirth;
+	User* prev;
+	User* next;
+};
+struct Course {
+	string id;
+	string courseName;
+	string teacherName;
+	int credits;
+	int maxStudents = 50;
+	int numberRegistered = 0;
+	int academicYear;
+	string wDay;
+	string session[2];
+	Course* prev;
+	Course* next;
+};
+struct ListCourses {
+	Course* head;
+	Course* tail;
+	Date startDate, endDate;
+	int size;
+};
+struct ListUser {
+	User* head;
+	User* tail;
+	int size;
+};
+struct CourseMark {
+	float otherMark = 0;
+	float midtermMark = 0;
+	float finalMark = 0;
+	float totalMark = 0;
+};
+struct SemesterMark {
+	float GPA = 0;
+	float overallGPA = 0;
+};
+struct Student {
+	string studentID;
+	string lastName;
+	string firstName;
+	string gender;
+	string socialID;
+	Date dateOfBirth;
+	int academicYear;
+	ListCourses enrolledCourses;
+	CourseMark courseMark;
+	SemesterMark semesterMark;
+	Student* prev;
+	Student* next;
+};
+struct ListStudent {
+	Student* head;
+	Student* tail;
+	string program;
+	string className;
+	string year;
+	int academicYear;
+	int size;
+};
+struct Semester {
+	int semester;
+	Date startDate, endDate;
+};
+struct Class {
+	string className;
+	fs::path path;
+	Class* prev;
+	Class* next;
+};
+struct ListClasses {
+	Class* head;
+	Class* tail;
+	int size;
+};
+
