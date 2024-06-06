b6f230bc89f48a6e84829f5e6bcbdb8b47486b82
NguyenSonTung16 <162577055+NguyenSonTung16@users.noreply.github.com>
2024-06-06 14:23:27 +0700

    sửa project.h

--- projktlt/projktlt/project.h
+++ projktlt/projktlt/project.h
@@ -1,41 +1,15 @@
 #ifndef _PROJECT_H
 #define _PROJECT_H
-#include <filesystem>
 #include <iostream>
 #include <cstring>
 #include <string>
 using namespace std;
-namespace fs = std::filesystem;
+
 struct Date {
 	int year;
 	int month;
 	int day;
 	string wDay;
-};
-struct SinhVien
-{
-	string mssv;
-	string ten;
-	string ho;
-	string gender;
-	int ngay, thang, nam;
-	string cccd;
-	SinhVien* next;
-};
-struct Listsv
-{
-	SinhVien* phead;
-	SinhVien* ptail;
-};
-struct Lop
-{
-	Listsv* ds;
-	char tenlop[20];
-	Lop* next;
-};
-struct danhsachcaclop 
-{
-	Lop* head;
 };
 struct Course
 {
@@ -48,46 +22,70 @@ struct Course
 	int academicYear;
 	string wDay;
 	string session[2];
-	Course* prev;
 	Course* next;
-	Listsv* dssv;
 };
-struct ListCourses
+struct 	ListCourses
+
 {
 	Course* head;
 	Course* tail;
 	Date startDate, endDate;
 	int size;
 };
-struct hocky
+struct SinhVien
+{
+	string mssv;
+	string ten;
+	string ho;
+	string gender;
+	Date birth;
+	string cccd;
+	SinhVien* next;
+	ListCourses* Lc;
+};
+struct ListSV
+{
+	SinhVien* phead;
+	SinhVien* ptail;
+};
+struct Class
+{
+	ListSV* ds;
+	string ClassName;
+	Class* next;
+};
+struct ListClass
+{
+	Class* head;
+};
+struct Semester
 {
 	int thutu;
-	char ngaybatdau[11];
-	char ngayketthuc[11];
-	char chonhocky[30];
-	hocky* next;
+	string Ngaybatdau;
+	string Ngayketthuc;
+	Semester* next;
 	ListCourses* lc;
 };
 
-struct namhoc
+struct NamHoc
 {
-	char thoigiannamhoc[30];
-	hocky* Hocky;
-	namhoc* next;
+	string ngaybatdau;
+	Semester* Hocky;
+	NamHoc* next;
 };
-struct listnamhoc
+struct ListNamHoc
 {
-	namhoc* phead;
+	NamHoc* phead;
 };
 
 
 bool kiemtrangaysinh(int ngay, int thang, int nam);
 void taosv(string &MSSV, string& ten, string& ho, string& gender, int& ngay, int& thang, int& nam, string &cccd);
-void themsvvaolop(Lop*& lop, string &MSSV, string& ten, string &ho, string& gender, int& ngay, int& thang, int& nam, string& cccd);
-void taonamhoc(listnamhoc& L, namhoc*& N);
-void taohocky(hocky*& H, listnamhoc& L);
-void themlopmoivaodanhsachcaclop(Lop*& CTT, danhsachcaclop*& DS);
-void xemdanhsachcaclop(danhsachcaclop*& DS);
+void themsvvaolop(Class*& lop, string &MSSV, string& ten, string &ho, string& gender, int& ngay, int& thang, int& nam, string& cccd);
+void taonamhoc(ListNamHoc& L, NamHoc*& N);
+void taohocky(Semester*& H, ListNamHoc& L);
+void themlopmoivaodanhsachcaclop(Class*& CTT,ListClass*& DS);
+void xemdanhsachcaclop(ListClass*& DS);
 void xemdanhsachkhoahoc(ListCourses*& LC);
 void xoasvkhoikhoahoc(ListCourses*& LC);
 //void xembangdiemcua1khoahoc(Student*& st, Course*& c, ListStudent*& lst);
