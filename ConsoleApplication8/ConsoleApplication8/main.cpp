b60bc662086ba7fa0d13011836cdb98a0ccf2fb5
NguyenSonTung16 <162577055+NguyenSonTung16@users.noreply.github.com>
2024-06-11 20:56:14 +0700

    Update main.cpp

--- ConsoleApplication8/ConsoleApplication8/main.cpp
+++ ConsoleApplication8/ConsoleApplication8/main.cpp
@@ -1,19 +1,15 @@
-﻿#include "raylib.h"
-#include <string.h>
-#include <ctype.h> // Thêm thư viện này để sử dụng hàm tolower
 #include "function.h"
-void DrawButton(Rectangle button, const char* text, bool mouseOverButton) {
-    DrawRectangleRec(button, mouseOverButton ? DARKGRAY : LIGHTGRAY);
-    DrawText(text, button.x + button.width / 2 - MeasureText(text, 20) / 2, button.y + button.height / 2 - 10, 20, BLACK);
-}
+
 int main() {
     const int screenWidth = 800;
     const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "");
 
-    // Khởi tạo cửa sổ đăng nhập
-    InitWindow(screenWidth, screenHeight, "Login Interface Example");
-
-    // Tạo các biến để lưu tên người dùng và mật khẩu
+    ListCourses LC;
+    InitList(LC);
+    ListNamHoc LNH = { NULL };
+    NamHoc* N = NULL;
+    Semester* sm = NULL;
     char username[128] = "\0";
     char password[128] = "\0";
     bool mouseOnText = false;
@@ -21,21 +17,16 @@ int main() {
     bool passwordBoxActive = false;
     bool loginAttempted = false;
     bool loginSuccessful = false;
-    bool showError = false; // Biến này chỉ hiển thị thông báo lỗi một lần
+    bool showError = false;
 
-    // Vị trí và kích thước các ô nhập liệu
     Rectangle usernameBox = { screenWidth / 2 - 100, screenHeight / 2 - 50, 200, 40 };
     Rectangle passwordBox = { screenWidth / 2 - 100, screenHeight / 2 + 10, 200, 40 };
     Rectangle loginButton = { screenWidth / 2 - 50, screenHeight / 2 + 70, 100, 40 };
 
     SetTargetFPS(60);
 
-    bool newWindowReady = false; // Biến để kiểm tra cửa sổ mới đã sẵn sàng hay chưa
-
     while (!WindowShouldClose()) {
         Vector2 mousePoint = GetMousePosition();
-
-        // Kiểm tra nếu chuột ở trên một ô nhập liệu
         mouseOnText = CheckCollisionPointRec(mousePoint, usernameBox) || CheckCollisionPointRec(mousePoint, passwordBox);
 
         if (mouseOnText) {
@@ -45,7 +36,6 @@ int main() {
             SetMouseCursor(MOUSE_CURSOR_DEFAULT);
         }
 
-        // Kiểm tra nhấp chuột vào ô nhập liệu
         if (CheckCollisionPointRec(mousePoint, usernameBox) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
             usernameBoxActive = true;
             passwordBoxActive = false;
@@ -59,10 +49,9 @@ int main() {
             passwordBoxActive = false;
         }
 
-        // Xử lý nhập liệu cho ô đang hoạt động
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("LOGIN", screenWidth / 2 -50, screenHeight / 2 -200, 40, RED);
+        DrawText("LOGIN", screenWidth / 2 - 50, screenHeight / 2 - 200, 40, RED);
         DrawText("Username:", screenWidth / 2 - 100, screenHeight / 2 - 70, 20, DARKGRAY);
         DrawRectangleRec(usernameBox, LIGHTGRAY);
         if (usernameBoxActive) DrawRectangleLinesEx(usernameBox, 1, RED);
@@ -76,13 +65,12 @@ int main() {
         DrawRectangleRec(loginButton, LIGHTGRAY);
         DrawText("Login", screenWidth / 2 - 25, screenHeight / 2 + 80, 20, DARKGRAY);
 
-        // Hiển thị thông báo lỗi nếu có
         if (showError) {
             DrawText("Invalid username or password", screenWidth / 2 - MeasureText("Invalid username or password", 20) / 2, screenHeight / 2 + 120, 20, RED);
         }
 
-
         EndDrawing();
+
         if (usernameBoxActive) {
             int key = GetKeyPressed();
             if ((key >= 32) && (key <= 125)) {
@@ -126,14 +114,11 @@ int main() {
             }
         }
 
-        // Kiểm tra thông tin đăng nhập khi nhấn nút "Login"
         if (CheckCollisionPointRec(mousePoint, loginButton) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
             loginAttempted = true;
             if (!IsEmpty(username) && !IsEmpty(password)) {
                 if (CheckLogin(username, password)) {
                     loginSuccessful = true;
-                    // Sau khi đăng nhập thành công, thiết lập biến newWindowReady thành true
-                    newWindowReady = true;
                     break;
                 }
                 else {
@@ -143,37 +128,42 @@ int main() {
         }
     }
 
-    if (loginSuccessful && newWindowReady) { // Kiểm tra cả hai điều kiện
-        InitWindow(800, 600, "User Dashboard");
+    if (loginSuccessful) {
+        const int dashboardWidth = 1400;
+        const int dashboardHeight = 800;
+        InitWindow(dashboardWidth, dashboardHeight,"");
         SetTargetFPS(60);
+
         Rectangle buttons[6];
         const char* buttonLabels[6] = {
             "Create New School Year",
             "Create New Semester",
-            "Add New Class",
+            "Add Course",
             "View Classes",
             "Add Student to Class",
-            "Doi mat khau"
+            "View Course"
         };
-
+        bool ViewCourseActive = false;
+        char ViewCourseInput[128] = "\0";
         bool createNewSchoolYearActive = false;
         char schoolYearInput[128] = "\0";
-        Rectangle schoolYearInputBox = { screenWidth / 2 - 100, 450, 200, 40 };
+        bool ViewCourseInputBoxActive = false;
+        Rectangle ViewCourseInputBox = { dashboardWidth / 2 - 100, 250, 200, 40 };
+        Rectangle schoolYearInputBox = { dashboardWidth / 2 - 100, 450, 200, 40 };
         bool schoolYearInputBoxActive = false;
         bool semesterInputBoxActive = false;
         char semesterInput[128] = "\0";
         bool createNewSemesterActive = false;
-        Rectangle semesterInputBox = { screenWidth / 2 - 100, 400,200, 40 };
-        ListNamHoc L = { NULL };
-        NamHoc* N = NULL;
-        Semester* sm = NULL;
+        Rectangle semesterInputBox = { dashboardWidth / 2 - 100, 400, 200, 40 };
+        bool createNewCourseActive = false;
 
         for (int i = 0; i < 6; i++) {
-            buttons[i].x = (screenWidth - 400) / 2; // Căn giữa theo chiều ngang
-            buttons[i].y = 50 + i * 60; // Để lại khoảng trống 50 pixel phía trên và giữa các ô là 60 pixel
-            buttons[i].width = 400; // Chiều rộng của ô
+            buttons[i].x = (dashboardWidth - 400) / 2;
+            buttons[i].y = 50 + i * 60;
+            buttons[i].width = 400;
             buttons[i].height = 40;
         }
+
         while (!WindowShouldClose()) {
             Vector2 mousePoint = GetMousePosition();
             bool mouseOverButton[6] = { false };
@@ -194,15 +184,20 @@ int main() {
                     case 1:
                         createNewSemesterActive = true;
                         break;
-                        // Implement other cases as needed
+                    case 2:
+                        createNewCourseActive = true;
+                        break;
+                    case 5:
+                        ViewCourseActive = true;
+                        break;
                     }
                 }
             }
-
-            if (createNewSchoolYearActive) {
+            if (createNewSchoolYearActive)
+            {
                 DrawRectangleRec(schoolYearInputBox, LIGHTGRAY);
                 DrawText(schoolYearInput, schoolYearInputBox.x + 5, schoolYearInputBox.y + 10, 20, DARKGRAY);
-                DrawText("Enter School Year (e.g., 2020-2021):", schoolYearInputBox.x, schoolYearInputBox.y -20, 20, LIGHTGRAY);
+                DrawText("Enter School Year (e.g., 2020-2021):", schoolYearInputBox.x, schoolYearInputBox.y - 20, 20, LIGHTGRAY);
 
                 if (CheckCollisionPointRec(mousePoint, schoolYearInputBox) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                     schoolYearInputBoxActive = true;
@@ -227,29 +222,63 @@ int main() {
                 }
 
                 if (IsKeyPressed(KEY_ENTER)) {
-                    taonamhoc(L, N, schoolYearInput);
+                    taonamhoc(LNH, N, schoolYearInput);
                     createNewSchoolYearActive = false;
-                    memset(schoolYearInput, 0, sizeof(schoolYearInput)); // Reset input
+                    memset(schoolYearInput, 0, sizeof(schoolYearInput));
                 }
             }
-            if (createNewSemesterActive && IsWindowReady())
-            {
-                
-                    taohocky(sm, L);
-                    CloseWindow();
-                    createNewSemesterActive = false;
-                
 
+            if (createNewSemesterActive) 
+            {
+                taohocky(sm, LNH);
+                createNewSemesterActive = false;
             }
 
+            if (createNewCourseActive)
+            {
+                string id, CourseName, ClassName, GVName, wDay, Session;
+                int AcademicYear, Credits;
+                // Hiển thị giao diện nhập liệu và nhận giá trị đầu vào
+                ShowInputCoursePage(id, CourseName, ClassName, GVName, AcademicYear, Credits, wDay, Session);
+                // Tạo đối tượng Course với dữ liệu đã nhập
+                Course* newCourse = InputCourse(id, CourseName, ClassName, GVName, AcademicYear, Credits, wDay, Session);
+                AddCourse(LC, newCourse);
+                createNewCourseActive = false;
+                
+            }
+            if (ViewCourseActive)
+            {
+                // Tạo một mảng động để lưu trữ các đối tượng Course từ danh sách liên kết
+                // Đếm số lượng khóa học trong danh sách liên kết
+                int numRows = 0;
+                Course* tempCount = LC.head;
+                while (tempCount != nullptr) {
+                    numRows++;
+                    tempCount = tempCount->next;
+                }
 
+                // Tạo một mảng động để lưu trữ các đối tượng Course từ danh sách liên kết
+                Course* courseArray = new Course[numRows];
+                Course* temp = LC.head;
+
+                // Sao chép thông tin từ danh sách liên kết vào mảng
+                for (int i = 0; i < numRows && temp != nullptr; i++) {
+                    courseArray[i] = *temp;
+                    temp = temp->next;
+                }
+
+                // Gọi hàm DrawCourseTable với mảng đã tạo
+                DrawCourseTable(courseArray, numRows);
+
+                
+                
+
+                ViewCourseActive = false;
+            }
             EndDrawing();
         }
     }
+
     CloseWindow();
     return 0;
 }
-
-
-
-
