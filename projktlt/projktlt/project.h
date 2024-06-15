339058ff6a0f04e04edbe3b8bc8a8591ae3e0094
tinphan247 <151385025+tinphan247@users.noreply.github.com>
2024-06-15 15:38:31 +0700

    Update project.h

--- projktlt/projktlt/project.h
+++ projktlt/projktlt/project.h
@@ -1,11 +1,13 @@
-#ifndef function_h
-#define function_h
-#include "raylib.h"
+#ifndef _PROJECT_H
+#define _PROJECT_H
 #include <iostream>
+#include <iomanip>
 #include <cstring>
-#include<fstream>
 #include <string>
+#include<fstream>
 using namespace std;
+
+struct Class;
 struct Date {
 	int year;
 	int month;
@@ -24,6 +26,7 @@ struct Course
 	string wDay;
 	string session;
 	Course* next;
+	Class* lop;
 };
 struct 	ListCourses
 
@@ -42,7 +45,6 @@ struct SinhVien
 	string cccd;
 	SinhVien* next;
 	ListCourses* Lc;
-	string ClassName;
 };
 struct ListSV
 {
@@ -78,61 +80,26 @@ struct ListNamHoc
 {
 	NamHoc* phead;
 };
-struct User
-{
-	string id;
-	string pass;
-	string ten;
-	string ho;
-	string gender;
-	Date birth;
-	string ClassName;
-	string academicYear;
-	bool staff;
-	User* next;
-};
-struct ListUser
-{
-	User* phead;
-	User* ptail;
-};
-void DrawButton(Rectangle button, const char* text, bool mouseOverButton);
+
+
 bool kiemtrangaysinh(int ngay, int thang, int nam);
-void taosv(string& MSSV, string& ten, string& ho, string& gender, int& ngay, int& thang, int& nam, string& cccd);
-void themsvvaolop(Class*& lop, string& MSSV, string& ten, string& ho, string& gender, int& ngay, int& thang, int& nam, string& cccd);
-void taonamhoc(ListNamHoc& L, NamHoc*& N, const char* thoigiannamhoc);
+int dayofweek(int day, int month, int year);
+void taosv(string &MSSV, string& ten, string& ho, string& gender, int& ngay, int& thang, int& nam, string &cccd);
+void themsvvaolop(Class*& lop, string &MSSV, string& ten, string &ho, string& gender, int& ngay, int& thang, int& nam, string& cccd);
+void taonamhoc(ListNamHoc& L, NamHoc*& N);
 void taohocky(Semester*& H, ListNamHoc& L);
-void themlopmoivaodanhsachcaclop(Class*& CTT, ListClass*& DS);
+void themlopmoivaodanhsachcaclop(Class*& CTT,ListClass*& DS);
 void xemdanhsachcaclop(ListClass*& DS);
 void xemdanhsachkhoahoc(ListCourses*& LC);
 void xoasvkhoikhoahoc(ListCourses*& LC);
-void DrawNumbers(NamHoc*& H, ListNamHoc& L);
-void InitList(ListCourses& list);
-void AddCourse(ListCourses& List, Course* newCourse);
-void showCourseDetails(ListCourses& List, Course& course);
-void viewcourse(ListCourses List, Course*& courses, int& numRows);
-void RemoveCourse(ListCourses& List, Course* course);
-Course* Find_ID(ListCourses List, string id);
-
-Course* InputCourse(string id, string CourseName, string ClassName, string GVName, int AcademicYear, int Credits, string wDay, string Session);
-void ShowInputCoursePage(string& id, string& CourseName, string& ClassName, string& GVName, int& AcademicYear, int& Credits, string& wDay, string& Session);
+//void xembangdiemcua1khoahoc(Student*& st, Course*& c, ListStudent*& lst);
+//void xemdanhsachhocvientrongkhoa(Course*& c);
+//7
+void InitList(ListCourses &list);
+Course* InputCourse(string id,string CourseName,string ClassName,string GVName,int AcademicYear,int Credits,string wDay,string Session);
+void AddCourse(ListCourses& List,Course* newCourse);
+void OutputListCourses(ListCourses List);
+//yeu cau8 
 ListSV* addListSV(string path);
-void DrawSVTable(const SinhVien* sv, int numRows);
-void ShowStudentListWindow(ListSV* svList);
-void DrawStudentListFromData(ListSV* studentList, int numRows);
-ListUser* addListUser(const string& path);
-// Hàm kiểm tra nếu chuỗi rỗng
-bool IsEmpty(const char* str);
-// Hàm kiểm tra thông tin đăng nhập
-bool CheckLogin(const char* username, const char* password, bool& checkstaff, ListUser*& LU);
-void ChangePassword(ListUser* userList, const char* username, const char* password);
-void saveListUser(ListUser* LUR, const string& path);
-bool Login(ListUser* LUR, char* username, char* password, bool& checkstaff);
-void CreateNewSchoolYeabutton(bool& createNewSchoolYearActive, bool& schoolYearInputBoxActive, char* schoolYearInput, ListNamHoc& LNH, NamHoc*& N, Vector2 mousePoint);
-void AddStudentsbutton(char* AddsvInput, Rectangle AddsvInputBox, bool& AddsvInputBoxActive);
-User* timnodeuser(ListUser* LUR, string username, string password);
-void DisplayProfileInfo(User* user);
-void ShowImportCoursePage(string& pathC);
-void ImportCourseFile(ListCourses& List, string path);
-void CourseDashboard(int screenWidth, int screenHeight, ListCourses& LC);
-#endif // FUNCTION_H
+void add1StudentCourse(Course& cour, SinhVien& sv);
+#endif
